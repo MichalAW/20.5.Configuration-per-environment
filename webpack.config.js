9fb313c22ed2c6ff9f430104a38ade7e5d14fd50
MichalAW <michal.wantaeuco@gmail.com>
2019-06-13 21:33:31 +0200

    function

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,45 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader"
-            }
+module.exports = function() {
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
         },
-        {
-            test: /\.css$/,
-            use: [{
-                loader: 'style-loader'
+        module: {
+            rules: [{
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
             {
-                loader: 'css-loader',
-                options: {
-                    modules: true
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true
+                    }
                 }
+                ]
             }
             ]
-        }
-        ]
-    },
-    devServer: {
-        contentBase: './build',
-        publicPath: '/',
-        historyApiFallback: {
-            index: '/index.html'
         },
-        port: 3000,
-        hot: true,
-        compress: false,
-        disableHostCheck: true,
-    },
+        devServer: {
+            contentBase: './build',
+            publicPath: '/',
+            historyApiFallback: {
+                index: '/index.html'
+            },
+            port: 3000,
+            hot: true,
+            compress: false,
+            disableHostCheck: true,
+        },
+    }
 };
