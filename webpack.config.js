f343e173c37992de0b41d12188857fd134af727b
MichalAW <michal.wantaeuco@gmail.com>
2019-06-19 18:40:50 +0200

    add mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = function(env) {
     return {
+        mode: env.production ? 'production' : 'development',
         entry: './src/app.js',
         output: {
             path: env.path.resolve(__dirname, 'build'),
